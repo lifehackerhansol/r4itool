f7fd9995ae33dd081f42e3bda8606062e41115a8
lifehackerhansol <lifehackerhansol@ds-homebrew.com>
2023-08-15 23:39:33 -0700

    Main: swap iostream for cstdio

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,7 @@
 #include "R4i.hpp"
 
+#include <cstdio>
 #include <fstream>
-#include <iostream>
 #include <string>
 #include <vector>
 
@@ -51,19 +51,19 @@ static void parseROM(std::vector<u8> &buffer, std::span<u8> &arm9,
 
 int main(int argc, char const *const *argv) {
   if (argc < 3) {
-    std::cout << "USAGE: path/to/R4.dat path/to/R4iLoader.dat\n";
+    printf("USAGE: path/to/R4.dat path/to/R4iLoader.dat\n");
     return 1;
   }
 
   auto r4 = readFile(argv[1]);
   if (r4.empty()) {
-    std::cout << "ERROR: Could not load R4.dat!\n";
+    printf("ERROR: Could not load R4.dat!\n");
     return 2;
   }
 
   auto loader = readFile(argv[2]);
   if (loader.empty()) {
-    std::cout << "ERROR: Could not load R4iLoader.dat!\n";
+    printf("ERROR: Could not load R4iLoader.dat!\n");
     return 3;
   }
 
@@ -71,7 +71,7 @@ int main(int argc, char const *const *argv) {
   std::span<u8> arm7;
   parseROM(r4, arm9, arm7);
   if (arm9.empty() || arm7.empty()) {
-    std::cout << "ERROR: R4.dat parsing failed!\n";
+    printf("ERROR: R4.dat parsing failed!\n");
     return 4;
   }
 
@@ -79,63 +79,62 @@ int main(int argc, char const *const *argv) {
   std::span<u8> ldr7;
   parseROM(loader, ldr9, ldr7);
   if (ldr9.empty() || ldr7.empty()) {
-    std::cout << "ERROR: R4iLoader.dat parsing failed!\n";
+    printf("ERROR: R4iLoader.dat parsing failed!\n");
     return 5;
   }
 
   auto secretArea = findSecretArea(arm9);
   if (!secretArea) {
-    std::cout << "ERROR: Could not find secret area!\n";
+    printf("ERROR: Could not find secret area!\n");
     return 6;
   }
 
-  std::cout << "Secret area:\n";
-  std::cout << std::hex;
+  printf("Secret area:\n");
 
   // Checksum9
-  auto const checksum9 = decodeU16(secretArea->arm9chk);
-  auto const computed9 = genChecksum9(arm9);
-  std::cout << "- checksum9: " << checksum9 << '\n';
-  std::cout << "- computed: " << computed9 << '\n';
+  u16 checksum9 = decodeU16(secretArea->arm9chk);
+  u16 computed9 = genChecksum9(arm9);
+  printf("- checksum9: 0x%04X\n", checksum9);
+  printf("- computed:  0x%04X\n", computed9);
 
   if (checksum9 != computed9)
-    std::cout << "DOES NOT MATCH\n";
+    printf("DOES NOT MATCH\n");
 
-  std::cout << '\n';
+  printf("\n");
 
   // Checksum7
-  auto const checksum7 = decodeU16(secretArea->arm7chk);
-  auto const computed7 = genChecksum7(arm7);
-  std::cout << "- checksum7: " << checksum7 << '\n';
-  std::cout << "- computed: " << computed7 << '\n';
+  u16 checksum7 = decodeU16(secretArea->arm7chk);
+  u16 computed7 = genChecksum7(arm7);
+  printf("- checksum7: 0x%04X\n", checksum7);
+  printf("- computed: 0x%04X\n", computed7);
 
   if (checksum7 != computed7)
-    std::cout << "DOES NOT MATCH\n";
+    printf("DOES NOT MATCH\n");
 
-  std::cout << '\n';
+  printf("\n");
 
   // ChecksumLdr
-  auto const checksumLdr = decodeU16(secretArea->ldrchk);
-  auto const computedLdr = genChecksumLdr(ldr9, ldr7);
-  std::cout << "- checksumLdr: " << checksumLdr << '\n';
-  std::cout << "- computed: " << computedLdr << '\n';
+  u16 checksumLdr = decodeU16(secretArea->ldrchk);
+  u16 computedLdr = genChecksumLdr(ldr9, ldr7);
+  printf("- checksumLdr: 0x%04X\n", checksumLdr);
+  printf("- computed: 0x%04X\n", computedLdr);
 
   if (checksumLdr != computedLdr)
-    std::cout << "DOES NOT MATCH\n";
+    printf("DOES NOT MATCH\n");
 
-  std::cout << '\n';
+  printf("\n");
 
   // Area checksum
-  auto const checksumArea = secretArea->areachk;
-  auto const computedArea =
+  u16 checksumArea = secretArea->areachk;
+  u16 computedArea =
       genAreaChk(encodeU16(computed9), encodeU16(computed7), encodeU16(computedLdr));
-  std::cout << "- checksumArea: " << checksumArea << '\n';
-  std::cout << "- computed: " << computedArea << '\n';
+  printf("- checksumArea: 0x%04X\n", checksumArea);
+  printf("- computed: 0x%04X\n", computedArea);
 
   if (checksumArea != computedArea)
-    std::cout << "DOES NOT MATCH\n";
+    printf("DOES NOT MATCH\n");
 
-  std::cout << '\n';
+  printf("\n");
 
   // FIX.
   secretArea->arm9chk = encodeU16(computed9);
@@ -144,10 +143,10 @@ int main(int argc, char const *const *argv) {
   secretArea->areachk = computedArea;
 
   if (!writeFile("R4-fixed.dat", r4)) {
-    std::cout << "ERROR: Could not save R4-fixed.dat!\n";
+    printf("ERROR: Could not save R4-fixed.dat!\n");
     return 7;
   }
 
-  std::cout << "File fixed: R4-fixed.dat\n";
+  printf("File fixed: R4-fixed.dat\n");
   return 0;
 }
